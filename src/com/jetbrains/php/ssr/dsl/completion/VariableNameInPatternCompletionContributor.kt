343a63d18db16cd68d72827154aa29a6bd84eec5
Kirill Smelov <kirill.smelov@jetbrains.com>
2018-07-24 16:08:52 +0300

    Completion for variable names inside the pattern for variables as well

--- src/com/jetbrains/php/ssr/dsl/completion/VariableNameInPatternCompletionContributor.kt
+++ src/com/jetbrains/php/ssr/dsl/completion/VariableNameInPatternCompletionContributor.kt
@@ -8,12 +8,14 @@ import com.intellij.patterns.StandardPatterns
 import com.intellij.psi.PsiElement
 import com.intellij.util.ProcessingContext
 import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocComment
+import com.jetbrains.php.lang.lexer.PhpTokenTypes
 import com.jetbrains.php.lang.lexer.PhpTokenTypes.IDENTIFIER
 import com.jetbrains.php.lang.parser.PhpElementTypes.STATEMENT
 import com.jetbrains.php.lang.parser.PhpElementTypes.USE_LIST
 import com.jetbrains.php.lang.parser.PhpStubElementTypes.*
 import com.jetbrains.php.lang.psi.PhpPsiUtil
 import com.jetbrains.php.lang.psi.PhpPsiUtil.isOfType
+import com.jetbrains.php.lang.psi.elements.Variable
 import com.jetbrains.php.ssr.dsl.entities.ConstraintName
 import com.jetbrains.php.ssr.dsl.indexing.ConstraintRawData
 import com.jetbrains.php.ssr.dsl.indexing.parseVariables
@@ -22,18 +24,21 @@ import com.jetbrains.php.ssr.dsl.marker.get
 
 class VariableNameInPatternCompletionContributor: CompletionContributor() {
   init {
-    extend(CompletionType.BASIC, PlatformPatterns.psiElement(IDENTIFIER).withText(StandardPatterns.string().startsWith("_")), VariableNamesInPatternCompletionProvider());
+    extend(CompletionType.BASIC, PlatformPatterns.psiElement(IDENTIFIER).withText(StandardPatterns.string().startsWith("_")), VariableNamesInPatternCompletionProvider(false))
+    extend(CompletionType.BASIC, PlatformPatterns.psiElement(PhpTokenTypes.VARIABLE).withParent(Variable::class.java).withText(StandardPatterns.string().startsWith("\$_")), VariableNamesInPatternCompletionProvider(true))
   }
 }
 
-class VariableNamesInPatternCompletionProvider : CompletionProvider<CompletionParameters>() {
+class VariableNamesInPatternCompletionProvider(val inVariable: Boolean) : CompletionProvider<CompletionParameters>() {
   override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext?, result: CompletionResultSet) {
     val topStatement = PhpPsiUtil.getParentByCondition<PsiElement>(parameters.position) { it.isTopStatement() } ?: return
     val ssrDocComment = PhpPsiUtil.getPrevSiblingByCondition<PhpDocComment>(topStatement) { it is PhpDocComment && it.ssrDoc() } ?: return
     for (variable in ssrDocComment.parseVariables()) {
-      result.addElement(LookupElementBuilder.create("_" + variable.name).withPresentableText(variable.name).withIcon(AllIcons.Nodes.Variable))
+      result.addElement(LookupElementBuilder.create(createLookupString(variable)).withPresentableText(variable.name).withIcon(AllIcons.Nodes.Variable))
     }
   }
+
+  private fun createLookupString(variable: ConstraintRawData) = if (inVariable) "\$_" + variable.name else "_" + variable.name
 }
 
 private val ConstraintRawData.name: String
