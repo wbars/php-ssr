d0b01b55e6879469a5bae986f036f4513c99ad2e
Kirill Smelov <kirill.smelov@jetbrains.com>
2018-08-10 17:10:06 +0300

    Lock inspection body because SSR engine is not thread-safe

--- src/com/jetbrains/php/ssr/dsl/inspections/StructuralSearchInspection.kt
+++ src/com/jetbrains/php/ssr/dsl/inspections/StructuralSearchInspection.kt
@@ -19,47 +19,54 @@ import com.jetbrains.php.ssr.dsl.indexing.TemplateIndex
 import com.jetbrains.php.ssr.dsl.indexing.TemplateRawData
 import com.jetbrains.php.ssr.dsl.indexing.TemplatesEnvironmentIndex
 import com.jetbrains.php.ssr.dsl.marker.buildConfiguration
+import java.util.concurrent.locks.ReentrantLock
 
 class StructuralSearchInspection : PhpInspection() {
+  companion object {
+    val lock = ReentrantLock()
+  }
   override fun buildVisitor(holder: ProblemsHolder, onTheFly: Boolean): PsiElementVisitor {
     return object : PhpElementVisitor() {
       val matcher = Matcher(holder.manager.project)
 
       override fun visitElement(element: PsiElement?) {
-        if (element == null) return
-        val settings = TemplatesEnvironmentIndex.getAllTemplatesSettings(holder.manager.project)
-        val excludedTemplatesNames = settings.filter { it.severity == Severity.OFF }.map { it.templateName }.toSet()
-        val configurationsRawData = getIncludedTemplateNames(element.project, excludedTemplatesNames).mapNotNull {
-          TemplateIndex.findTemplateRawData(element.project, it)
-        }
-        val configurations = configurationsRawData.map { it.buildConfiguration(element.project) }
-        val cache: Map<Configuration, MatchContext> = mutableMapOf()
-        try {
-          matcher.precompileOptions(configurations, cache)
-        } catch (ignored: Exception) {
-          return
-        }
-        val matchedNodes = SsrFilteringNodeIterator(element)
-        for (configuration in configurations) {
-          val configurationSetting = settings.find { it.templateName.toUpperCase() == configuration.name.toUpperCase() }
-          val scope: SearchScope? = getSearchScope(configurationSetting, configurationsRawData, configuration.name)
-          if (scope != null && !scope.createScope(element.project).contains(element.containingFile.virtualFile)) continue
-          val severity: Severity? = getSeverity(configurationSetting, configurationsRawData, configuration.name)
-          if (severity == Severity.OFF) continue
-          val context = cache[configuration] ?: continue
-          if (Matcher.checkIfShouldAttemptToMatch(context, matchedNodes)) {
-            matcher.processMatchesInElement(context, configuration,
-                                            CountingNodeIterator(context.pattern.nodeCount, matchedNodes)) { matchResult, c ->
-              if (severity != null && severity != Severity.DEFAULT) {
-                holder.registerProblem(matchResult.match, c.name, severity.highlightType)
-              }
-              else {
-                holder.registerProblem(matchResult.match, c.name)
-              }
-              true
-            }
-            matchedNodes.reset()
+        synchronized(lock) {
+          if (element == null) return
+          val settings = TemplatesEnvironmentIndex.getAllTemplatesSettings(holder.manager.project)
+          val excludedTemplatesNames = settings.filter { it.severity == Severity.OFF }.map { it.templateName }.toSet()
+          val configurationsRawData = getIncludedTemplateNames(element.project, excludedTemplatesNames).mapNotNull {
+            TemplateIndex.findTemplateRawData(element.project, it)
           }
+          val configurations = configurationsRawData.map { it.buildConfiguration(element.project) }
+          val cache: Map<Configuration, MatchContext> = mutableMapOf()
+          try {
+            matcher.precompileOptions(configurations, cache)
+          } catch (ignored: Exception) {
+            return
+          }
+          val matchedNodes = SsrFilteringNodeIterator(element)
+          for (configuration in configurations) {
+            val configurationSetting = settings.find { it.templateName.toUpperCase() == configuration.name.toUpperCase() }
+            val scope: SearchScope? = getSearchScope(configurationSetting, configurationsRawData, configuration.name)
+            if (scope != null && !scope.createScope(element.project).contains(element.containingFile.virtualFile)) continue
+            val severity: Severity? = getSeverity(configurationSetting, configurationsRawData, configuration.name)
+            if (severity == Severity.OFF) continue
+            val context = cache[configuration] ?: continue
+            if (Matcher.checkIfShouldAttemptToMatch(context, matchedNodes)) {
+              matcher.processMatchesInElement(context, configuration,
+                                              CountingNodeIterator(context.pattern.nodeCount, matchedNodes)) { matchResult, c ->
+                if (severity != null && severity != Severity.DEFAULT) {
+                  holder.registerProblem(matchResult.match, c.name, severity.highlightType)
+                }
+                else {
+                  holder.registerProblem(matchResult.match, c.name)
+                }
+                true
+              }
+              matchedNodes.reset()
+            }
+          }
+
         }
       }
     }
